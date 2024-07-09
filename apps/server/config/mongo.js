5925705f121107bf76a61ab33b31e72e68fb20a6
Juan <arzzdeveloper04@gmail.com>
2024-07-09 04:32:26 -0600

    Update mongo.js

--- apps/server/config/mongo.js
+++ apps/server/config/mongo.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const { MONGO_URI } = require('../config/constants');
-const { URI }   = "mongodb+srv://arzz:<password>@sm-db.0z7r5tf.mongodb.net/?retryWrites=true&w=majority&appName=sm-db";
+const { URI }   = "mongodb+srv://arzz:0LSxlXz8MO48UOoV@sm-db.0z7r5tf.mongodb.net/?retryWrites=true&w=majority&appName=sm-db"
 
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect( URI, clientOptions);
+        await mongoose.connect( MONGO_URI, clientOptions);
         console.log('Database connected');
     } catch (error) {
         console.error('Database connection error:', error);
