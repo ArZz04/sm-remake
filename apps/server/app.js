d37914e7c6427fe5df03430d861c6c89c2c793a5
Juan Arvizu <arzzdeveloper04@gmail.com>
2024-07-17 05:12:01 -0600

    Update app.js

--- apps/server/app.js
+++ apps/server/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 var corsOptions = {
-    origin: ["https://web-sm.netlify.app", "http://127.0.0.1:5500", "https://arzz04.github.io/sm-remake"],
+    origin: ["https://web-sm.netlify.app", "http://127.0.0.1:5500", "https://arzz04.github.io"],
     optionsSuccessStatus: 200 // For legacy browser support
 }
 
