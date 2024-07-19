808c48ce2a93a815270a7b9af45db9beb9eb3e8e
Juan <arzzdeveloper04@gmail.com>
2024-07-18 19:12:58 -0600

    ordered

--- apps/web/public/components/componentCerdo1.js
+++ apps/web/public/components/componentCerdo1.js
@@ -10,10 +10,10 @@ class Screen {
             productElement.classList.add('product');
             productElement.innerHTML = `
                 <div class="product-info" style="display: flex; color: #1b1b1b">
-                    <p class="product-name text-40 m0">${product.name}</p>
-                    <p class="product-dots text-35 m0">${'.'.repeat(product.dots)}</p>
-                    <p class="product-price text-40 m0">$${product.price}</p>
-                    <p class="product-format text-30 m0">${product.format}</p>
+                    <p class="product-name m0">${product.name}</p>
+                    <p class="product-dots m0">${'.'.repeat(product.dots)}</p>
+                    <p class="product-price m0">$${product.price}</p>
+                    <p class="product-format m0">${product.format}</p>
                 </div>
             `;
             container.appendChild(productElement);
