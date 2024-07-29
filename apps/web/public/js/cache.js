a92f567f688b1d5ca6d43f10388978d6b0a7133a
Juan <arzzdeveloper04@gmail.com>
2024-07-29 17:44:02 -0600

    update-cache

--- apps/web/public/js/cache.js
+++ apps/web/public/js/cache.js
@@ -11,10 +11,10 @@ export async function fetchDataAndUpdateCache(apiUrl, cacheName) {
             const data = await response.json();
             const apiData = JSON.stringify(data);
 
-            const cachedResponse = await getCache(cacheName, apiUrl);
+            const cachedResponse = getCache(cacheName, apiUrl);
 
             if (!cachedResponse || cachedResponse !== apiData) {
-                await setCache(cacheName, apiUrl, apiData);
+                setCache(cacheName, apiUrl, apiData);
                 return data;  // Devuelve los datos actualizados
             } else {
                 return JSON.parse(cachedResponse);  // Devuelve los datos actuales del caché
@@ -26,7 +26,7 @@ export async function fetchDataAndUpdateCache(apiUrl, cacheName) {
         console.error('Error en la solicitud a la API:', error);
 
         // Si hay un error en la conexión, usa los datos almacenados en el caché
-        const cachedResponse = await getCache(cacheName, apiUrl);
+        const cachedResponse = getCache(cacheName, apiUrl);
         if (cachedResponse) {
             return JSON.parse(cachedResponse);  // Devuelve los datos almacenados en el caché
         } else {
@@ -36,22 +36,14 @@ export async function fetchDataAndUpdateCache(apiUrl, cacheName) {
 }
 
 // Función para obtener los datos del caché
-async function getCache(cacheName, url) {
-    const cache = await caches.open(cacheName);
-    const response = await cache.match(url);
-    if (response) {
-        return response.text();
-    }
-    return null;
+function getCache(cacheName, url) {
+    const cacheKey = `${cacheName}-${url}`;
+    return localStorage.getItem(cacheKey);
 }
 
 // Función para establecer los datos en el caché
-async function setCache(cacheName, url, data) {
-    const cache = await caches.open(cacheName);
-    const response = new Response(data, {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    await cache.put(url, response);
+function setCache(cacheName, url, data) {
+    const cacheKey = `${cacheName}-${url}`;
+    localStorage.setItem(cacheKey, data);
 }
+
