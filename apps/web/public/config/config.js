ee2517a3cccc4a672088053ade0e45ed2bb25a73
Juan <arzzdeveloper04@gmail.com>
2024-08-10 16:28:05 -0600

    Update config.js

--- apps/web/public/config/config.js
+++ apps/web/public/config/config.js
@@ -1,26 +1,50 @@
-
-import { getSchendule } from '../js/api.js';
+import { getSchedule, updateText } from '../js/api.js';
 
 const labelApiStatus = document.getElementById('api-status');
-
 const tUp = document.getElementById('tUp');
 const tDown = document.getElementById('tDown');
-const btSave = document.getElementById('btn-schendule');
+const btSave = document.getElementById('btn-schedule');
 
-async function fetchSchendule() {
+async function fetchSchedule() {
     try {
-
-        labelApiStatus.innerText = '  Waiting';
+        labelApiStatus.textContent = '  Waiting';
         labelApiStatus.style.color = 'orange'; 
 
-        const data = await getSchendule();
+        const data = await getSchedule();
+        if (data && data.length > 0) {
+            labelApiStatus.textContent = '  Online';
+            labelApiStatus.style.color = 'green';
 
+            const schedule = data[0]; // Assuming you want the first schedule
+            tUp.value = schedule.textUp || '';
+            tDown.value = schedule.textDown || '';
+        } else {
+            labelApiStatus.textContent = '  No data available';
+            labelApiStatus.style.color = 'gray';
+        }
     } catch (error) {
-        labelApiStatus.innerText = '  Offline';
+        labelApiStatus.textContent = '  Offline';
         labelApiStatus.style.color = 'red';
-        console.error('Error al obtener productos:', error);
+        console.error('Error al obtener el horario:', error);
     }
 }
 
-// Llama a la función para obtener los productos más recientes
-fetchSchendule();
+btSave.addEventListener('click', async (event) => {
+    event.preventDefault(); // Previene el envío del formulario y la recarga de la página
+
+    const updatedTextData = {
+        textUp: tUp.value,
+        textDown: tDown.value
+    };
+
+    try {
+        const data = await updateText(updatedTextData);
+        console.log('Texto actualizado:', data);
+        fetchSchedule();
+    } catch (error) {
+        console.error('Error al actualizar el texto:', error);
+    }
+});
+
+// Llama a la función para obtener el horario más reciente
+fetchSchedule();
